e84abf126005b9cd9ffd8dbd37de383bb0d7523d
Clayton Rooke <claytonjrooke@gmail.com>
2019-03-15 17:56:12 -0400

    added base functionality for endgameJS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # endgameJS
+A node server that texts you when Avengers: Endgame tickets are released in Canada.
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3000;
+const request = require('request');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/endgame', (req, res) => {
+    var options = { method: 'GET',
+        url: 'https://www.cineplex.com/Movie/avengers-endgame',
+        headers:
+            { 'Postman-Token': '433fd9ce-0c5e-4762-8cf3-2be3d7880337',
+                'cache-control': 'no-cache' } };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        let endgameOut = false;
+
+        page = body.toString();
+
+        //console.log(page);
+
+        endgameOut = !page.includes("<div class=\"h3 text-highlight text-center text-bold fill-width position-absolute absolute-align-center pad-md\">Tickets for this movie are not available at this moment</div>");
+
+        res.send(endgameOut ? "Endgame is out! Go buy tickets" : "Endgame is not out yet");
+    });
+
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
